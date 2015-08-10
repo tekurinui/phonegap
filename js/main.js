e7692d8734f6e074e0ec27481fa822ac3b70dfd1
Jeremy Banks <jeremy@plink.co.nz>
2015-08-10 23:39:00 +0000

    Part 8

--- js/main.js
+++ js/main.js
@@ -5,6 +5,25 @@ var app = {
         this.store = new MemoryStore(function(){
             $('body').html(new HomeView(self.store).render().el);
         });
+        this.registerEvents();
+    },
+    
+    registerEvents: function(){
+        if (document.documentElement.hasOwnProperty('ontouchstart')){
+            $('body').on('touchstart', 'a', function(event){
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event){
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            $('body').on('mousedown', 'a', function(event){
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event){
+                $(event.target).removeClass('tappable-active');
+            });
+        }
     },
 
     showAlert: function(message, title){
