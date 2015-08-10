5b0100f2017eca35d83dabb7c5b471922f2e69af
Jeremy Banks <jeremy@plink.co.nz>
2015-08-10 22:55:06 +0000

    Part 7

--- js/main.js
+++ js/main.js
@@ -1,34 +1,10 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function(){
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
-    },
-    
-    renderHomeView: function(){
-        var html =
-            "<div class='header'><h1>Kāinga</h1></div>" +
-            "<div class='search-view'>" +
-            "<input class='search-key'/>" +
-            "<ul class='employee-list'></ul>" +
-            "</div>";
-        $('body').html(html);
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     showAlert: function(message, title){
