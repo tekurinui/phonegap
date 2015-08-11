90e68bbba56e275c9b83ea81d12405e2ff6f30cc
Jeremy Banks <jeremy@plink.co.nz>
2015-08-11 00:38:12 +0000

    Part 11

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -2,6 +2,7 @@ var EmployeeView = function(employee){
     this.initialise = function(){
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.add-contact-btn',  this.addToContacts);
     };
     
     this.render = function(){
@@ -19,6 +20,23 @@ var EmployeeView = function(employee){
             function(){
                 alert('Error getting location');
             });
+        return false;
+    };
+    
+    this.addToContacts = function(event){
+        event.preventDefault();
+        console.log('addToContacts');
+        if (!navigator.contacts){
+            app.showAlert('Contacts API not supported', 'Error');
+            return;
+        }
+        var contact = navigator.contacts.create();
+        contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
+        contact.phoneNumbers = phoneNumbers;
+        contact.save();
         return false;
     };
     
