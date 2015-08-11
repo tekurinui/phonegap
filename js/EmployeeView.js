0f9eba4faf0218abf1c8e08e11a6c02a823e5503
Jeremy Banks <jeremy@plink.co.nz>
2015-08-11 00:09:19 +0000

    Part 10

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -1,12 +1,27 @@
 var EmployeeView = function(employee){
     this.initialise = function(){
         this.el = $('<div/>');
+        this.el.on('click', '.add-location-btn', this.addLocation);
     };
     
     this.render = function(){
         this.el.html(EmployeeView.template(employee));
         return this;
-    }
+    };
+    
+    this.addLocation = function(event){
+        event.preventDefault();
+        console.log('adLocation');
+        navigator.geolocation.getCurrentPosition(
+            function(position){
+                $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+            },
+            function(){
+                alert('Error getting location');
+            });
+        return false;
+    };
+    
     this.initialise();
 };
 
