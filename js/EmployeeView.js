13940cb974ca7c25e9dbae466e2849e3ee775a1f
Jeremy Banks <jeremy@plink.co.nz>
2015-08-11 00:50:38 +0000

    Part 12

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -3,6 +3,7 @@ var EmployeeView = function(employee){
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
         this.el.on('click', '.add-contact-btn',  this.addToContacts);
+        this.el.on('click', '.change-pic-btn',  this.changePicture);
     };
     
     this.render = function(){
@@ -37,6 +38,28 @@ var EmployeeView = function(employee){
         phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
         contact.phoneNumbers = phoneNumbers;
         contact.save();
+        return false;
+    };
+    
+    this.changePicture = function(event){
+        event.preventDefault();
+        if (!navigator.camera){
+            app.showAlert('Camera API not supported', 'Error');
+            return;
+        }
+        var options = {
+            quality:         50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType:      1,
+            encodingType:    0
+        };
+        navigator.camera.getPicture(
+            function(imageData){
+                $('.employee-image', this.el).attr('src', 'data:image/jpeg:base64,' + imageData);
+            },
+            function(){
+                app.showAlert('Error taking picture', 'Error');
+            });
         return false;
     };
     
