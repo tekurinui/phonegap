438e73f5339e747c8ae32bb320dd1eece2524636
Jeremy Banks <jeremy@plink.co.nz>
2015-08-11 01:54:43 +0000

    Part 13

--- js/main.js
+++ js/main.js
@@ -30,17 +30,51 @@ var app = {
     },
     
     route: function(){
-        var hash = window.location.hash;
+        var hash = window.location.hash, self = this;
         if (!hash){
-            $('body').html(new HomeView(this.store).render().el);
+            if (this.homePage){
+                this.slidePage(this.homePage);
+            } else {
+                this.homePage = new HomeView(this.store).render();
+                this.slidePage(this.homePage);
+            }
             return;
         }
         var match = hash.match(app.detailsURL);
         if (match){
             this.store.findById(Number(match[1]), function(employee){
-               $('body').html(new EmployeeView(employee).render().el); 
+               self.slidePage(new EmployeeView(employee).render()); 
             });
         }
+    },
+    
+    slidePage: function(page){
+        var currentPageDest, self = this;
+        
+        if (!this.currentPage){
+            $(page.el).attr('class', 'page stage-center');
+            $('body').append(page.el);
+            this.currentPage = page;
+            return;
+        }
+        
+        $('.stage-right, .stage-left').not('.homePage').remove();
+        
+        if (page === app.homePage){
+            $(page.el).attr('class', 'page stage-left');
+            currentPageDest = 'stage-right';
+        } else {
+            $(page.el).attr('class', 'page stage-right');
+            currentPageDest = 'stage-left';
+        }
+        
+        $('body').append(page.el);
+        
+        setTimeout(function(){
+            $(self.currentPage.el).attr('class', 'page transition ' + currentPageDest);
+            $(page.el).attr('class', 'page stage-center transition');
+            self.currentPage = page;
+        });
     },
 
     showAlert: function(message, title){
